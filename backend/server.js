518f234dd6d5be57c8ea6cffff00bb798e277673
miller505 <joelgurrola.biz@gmail.com>
2026-02-26 22:24:56 -0600

    V2.6 BETA

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "http://localhost:5173",
+  "https://www.prontomx.com",
+  "https://prontomx.com",
   process.env.FRONTEND_URL,
 ];
 
